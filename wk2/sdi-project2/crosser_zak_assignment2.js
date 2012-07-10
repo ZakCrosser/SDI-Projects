acf251539c3d218cd5ecfa7b5b76a8ad3be28c7d
ZakCrosser <zakcrosser@gmail.com>
2012-07-09 19:22:56 -0600

    Story and variables

---
+++ wk2/sdi-project2/crosser_zak_assignment2.js
@@ -0,0 +1,32 @@
+//alert("JavaScript works!");
+/*  Zachary Crosser
+	06/06/2012
+	Deliverable 1,
+	Going to the Zoo
+*/
+
+var animals = [
+      "Elephant",
+      "Tiger",
+      "Monkey",
+      "Giraffe"
+    ],
+    goingToTheZoo = "We are going to the zoo today!",
+    faces = ["stuck out our tongue.", "started to laugh.", "bug eyed looked at them.", "wanted to ride them."],
+    leavingTheZoo = "Nooo we don't want to leave",
+    hoursSlept = 8,
+    wellSlept = true;
+    
+
+
+/*we we all excited when told we were going to the zoo
+we all wanted to see the elephants, tigers, monkeys, and the giraffes
+we did get to see all of the animals we wanted to see
+all of our emotions were the same when we saw the animals,
+when we saw the elephants animals we stuck are toung out at them
+when we saw the monkeys animals we started to laugh
+when we saw the tigers animals we all made bug eyed looks at them and were scared
+when we saw the giraffe animals we all wanted to ride them
+we were all sad when we had to leave
+when we got home dad asked us if we was the tigers we all said yes and told him how scary they were.
+*/
