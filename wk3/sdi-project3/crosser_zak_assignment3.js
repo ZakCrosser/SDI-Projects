be94a9116b22f2ce329e867dc860df50df62bb32
ZakCrosser <zakcrosser@gmail.com>
2012-07-16 16:27:45 -0600

    more function work/monday 2hrs

--- wk3/sdi-project3/crosser_zak_assignment3.js
+++ wk3/sdi-project3/crosser_zak_assignment3.js
@@ -38,3 +38,50 @@
     console.log("We made " + this.type + " faces at the " + animal.name);
   });
 
+// Zoo: Object
+  // Method Procedure: initialize
+  // Argument: Array
+  var Zoo = (function(animalNames) {
+    // Property: Array - animals
+    this.animals = new Array( animalNames.length );
+    // Property: Number - numberOfAnimals
+    this.numberOfAnimals = animalNames.length;
+    // For Loop
+    for( var i = 0; i < this.numberOfAnimals; i++ ) {
+      this.animals[i] = new Animal(animalNames[i]);
+    };
+    // Return Object
+  });
+  
+  // Method Function: hasAnimalsLeftToVisit
+  Zoo.prototype.hasAnimalsLeftToVisit = (function() {
+    // Local Varables
+    var hasAnimalsLeft = false;
+    // While Loop
+    for( var animalIndex = 0; animalIndex < this.animals.length; animalIndex++ ) {
+      // Conditional
+      if( !this.animals[animalIndex].has_been_visited() ) {
+        hasAnimalsLeft = true;
+        break;
+      };
+    };
+    // Return Boolean
+    return hasAnimalsLeft;
+  });
+  
+  // Method Function: numberOfVisitedAnimals
+  Zoo.prototype.numberOfVisitedAnimals = (function() {
+    // Local variables
+    var visitedAnimalsCount = 0;
+    // For Loop
+    for( var animalsIndex = 0; animalsIndex < this.animals.length; animalsIndex++ ) {
+      // Nested Conditional
+      if( this.animals[animalsIndex].has_been_visited() ) {
+        // Math
+        visitedAnimalsCount++;
+      }
+    };
+    // Return Number
+    return visitedAnimalsCount;
+  });
+
